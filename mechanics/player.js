cdb8a1a77865deb2afaba900c40066d2a731fa33
JordanButler <jbutle10@uoregon.edu>
2015-02-11 08:37:54 -0800

    added sprite animation data to player.js

--- mechanics/player.js
+++ mechanics/player.js
@@ -14,22 +14,82 @@ var Player = function(x, y) {
 		"frames": {
 			"width": 32,
 			"height": 32,
-			"count": 1
+			"count": 16
 		},
 		"animations": {
-			"stand": {
+			"stand-front": {
 				"frames": [0],
-				"next": "stand",
+				"next": "stand-front",
 				"speed": 1
 			},
-			"walking": {
-				"frames": [0],
-				"next": "walking",
+			"stand-left": {
+				"frames": [4],
+				"next": "stand-left",
+				"speed": 1
+			},
+			"stand-right": {
+				"frames": [8],
+				"next": "stand-right",
+				"speed": 1
+			},
+			"stand-back": {
+				"frames":[12],
+				"next": "stand-back",
+				"speed": 1
+			},
+			"spin-left": {
+				"frames": [0,8,12,4],
+				"next": "spin",
+				"speed": 1
+			},
+			"spin-right": {
+				"frames": [0,4,12,8],
+				"next": "spin",
+				"speed": 1
+			},
+			"walk-front": {
+				"frames": [1,2,3,2],
+				"next": "walk-front",
+				"speed": 1
+			},
+			"walk-left": {
+				"frames": [5,6,7,6],
+				"next": "walk-left",
+				"speed": 1
+			},
+			"walk-right": {
+				"frames": [9,10,11,10],
+				"next": "walk-right",
+				"speed": 1
+			},
+			"walk-back": {
+				"frames": [13,14,15,14],
+				"next": "walk-back",
+				"speed": 1
+			},
+			"run-front": {
+				"frames": [1,3],
+				"next": "run-front",
+				"speed": 1
+			},
+			"run-left": {
+				"frames": [5,6,7,6],
+				"next": "run-left",
+				"speed": 3
+			},
+			"run-right": {
+				"frames": [9,10,11,10],
+				"next": "run-right",
+				"speed": 3
+			},
+			"run-back": {
+				"frames": [13,15],
+				"next": "run-back",
 				"speed": 1
 			}
 		}
 	});
-	this.animations = new createjs.Sprite(this.spriteSheet, "stand"); // change the second string to an animation from the spritesheet
+	this.animations = new createjs.Sprite(this.spriteSheet, "run-front"); // change the second string to an animation from the spritesheet
 	this.animations.x = this.x;
 	this.animations.y = this.y;
 	this.watchedElements = [];
