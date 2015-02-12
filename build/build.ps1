ee0448cb00bbd043536bcef27096ec927193d59c
JordanButler <jbutle10@uoregon.edu>
2015-02-12 09:12:50 -0800

    added powershell build script

---
+++ build/build.ps1
@@ -0,0 +1,17 @@
+java -jar compiler.jar `
+	--js=../libs/easeljs-0.8.min.js --js=../libs/preload-0.6.min.js --js=../libs/astar.js `
+	`
+	--js=../classes/complexobject.class.js --js=../classes/simpleobject.class.js --js=../classes/person.class.js `
+	`
+	--js=../complexobjects/examplecomplexobject.js `
+	`
+	--js=../simpleobjects/examplesimpleobject.js `
+	`
+	--js=../maps/map1.js `
+	`
+	--js=../mechanics/renderer.js --js=../mechanics/player.js --js=../mechanics/collision.js `
+	--js=../mechanics/enemy.js  --js=../mechanics/constants.js --js=../mechanics/main.js `
+	`
+	--js=../rules/playerturn.js `
+	`
+	--js_output_file=game.min.js
